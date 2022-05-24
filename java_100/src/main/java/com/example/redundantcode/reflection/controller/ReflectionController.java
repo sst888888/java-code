20ead3403a30aa137fc00bc74fae0a8e9e1af3f8
cp <4525999888@qq.com>
2022-05-24 14:50:46 +0800

    dd reflection

---
+++ java_100/src/main/java/com/example/redundantcode/reflection/controller/ReflectionController.java
@@ -0,0 +1,41 @@
+package com.example.redundantcode.reflection.controller;
+
+
+import com.example.redundantcode.reflection.right.BetterBankService;
+import com.example.redundantcode.reflection.wrong.BankService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+@Slf4j
+@RestController
+@RequestMapping("reflection")
+public class ReflectionController {
+
+    @PostMapping("/bank/createUser")
+    public String createUser(@RequestBody String data) {
+        log.info("createUser called with argument {}", data);
+        return "createUser ok";
+    }
+
+    @PostMapping("/bank/pay")
+    public String pay(@RequestBody String data) {
+        log.info("pay called with argument {}", data);
+        return "pay ok";
+    }
+
+    @GetMapping("wrong")
+    public void wrong() throws IOException {
+        BankService.createUser("zhuye", "xxxxxxxxxxxxxxxxxx", "13612345678", 36);
+        BankService.pay(1234L, new BigDecimal("100.5"));
+
+    }
+
+    @GetMapping("right")
+    public void right() throws IOException {
+       BetterBankService.createUser("zhuye", "xxxxxxxxxxxxxxxxxx", "13612345678", 36);
+       BetterBankService.pay(1234L, new BigDecimal("100.5"));
+    }
+}
---
+++ java_100/src/main/java/com/example/redundantcode/reflection/right/AbstractAPI.java
@@ -0,0 +1,8 @@
+package com.example.redundantcode.reflection.right;
+
+/**
+ * 因为这个案例中的接口并没有公共数据可以抽象放到基类 https://time.geekbang.org/column/article/228964
+ */
+public abstract class AbstractAPI {
+
+}
