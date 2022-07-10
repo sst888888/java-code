5b6523c2c2fbe97cb8cdfca5f8adf1e787b43554
cp <4525999888@qq.com>
2022-07-10 15:51:56 +0800

    add

--- java_base/src/main/java/string/Jtest.java
+++ java_base/src/main/java/string/Jtest.java
@@ -11,11 +11,25 @@ public class Jtest {
         oper(a, b);
 
         System.out.print(a + "," + b);
+
+        test();
     }
 
     static void oper(StringBuffer c, StringBuffer d) {
         c.append("B");
         d = c;
+    }
+
+    static void test() {
+        // https://blog.csdn.net/u011069294/article/details/107640761
+        String s1 = "javaEEhadoop";
+        String s2 = "javaEE";
+        String s3 = s2 + "hadoop";
+        System.out.print(s1 == s3);
+
+        final String s4 = "javaEE";
+        String s5 = s4 + "hadoop";
+        System.out.print(s1 == s5);
     }
 
 }
