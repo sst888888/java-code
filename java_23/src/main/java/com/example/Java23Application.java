b620e9d73421eb7f894494e24ba1f1ef4fe7b846
cp <4525999888@qq.com>
2023-09-26 13:26:39 +0800

    design

---
+++ java_23/src/main/java/com/example/Java23Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java23Application.class, args);
+	}
+
+}
