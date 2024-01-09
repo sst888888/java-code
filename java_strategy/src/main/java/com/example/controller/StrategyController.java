cabce077d2e37af5975f4eddcdc1f2d5338caf03
cp <4525999888@qq.com>
2024-01-09 14:52:06 +0800

    design proxy ex1

--- java_strategy/src/main/java/com/example/controller/StrategyController.java
+++ java_strategy/src/main/java/com/example/controller/StrategyController.java
@@ -8,7 +8,6 @@ import com.example.release.ReleaseHandlerFactory;
 import com.example.risk.ReqVo;
 import com.example.risk.StrategyHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
