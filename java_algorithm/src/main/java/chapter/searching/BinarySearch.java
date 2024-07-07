b81dbc2f90be619f71fe93e5e0c192fd8cca1dcf
cp <4525999888@qq.com>
2024-07-07 20:58:40 +0800

    二分查找分治

--- java_algorithm/src/main/java/chapter/searching/BinarySearch.java
+++ java_algorithm/src/main/java/chapter/searching/BinarySearch.java
@@ -45,6 +45,34 @@ public class BinarySearch {
         return -1;
     }
 
+    // https://www.hello-algo.com/chapter_divide_and_conquer/binary_search_recur/#1
+    /* 二分查找：问题 f(i, j) */
+    static int dfs(int[] nums, int target, int i, int j) {
+        // 若区间为空，代表无目标元素，则返回 -1
+        if (i > j) {
+            return -1;
+        }
+        // 计算中点索引 m
+        int m = (i + j) / 2;
+        if (nums[m] < target) {
+            // 递归子问题 f(m+1, j)
+            return dfs(nums, target, m + 1, j);
+        } else if (nums[m] > target) {
+            // 递归子问题 f(i, m-1)
+            return dfs(nums, target, i, m - 1);
+        } else {
+            // 找到目标元素，返回其索引
+            return m;
+        }
+    }
+
+    /* 二分查找 */
+    static int binarySearch2(int[] nums, int target) {
+        int n = nums.length;
+        // 求解问题 f(0, n-1)
+        return dfs(nums, target, 0, n - 1);
+    }
+
     public static void main(String[] args) {
         int target = 6;
         int[] nums = {1, 3, 6, 8, 12, 15, 23, 26, 31, 35};
@@ -52,6 +80,9 @@ public class BinarySearch {
         System.out.println("目标元素6的索引 = " + index);
 
         index = binarySearchLCR0(nums, target);
+        System.out.println("目标元素6的索引 = " + index);
+
+        index = binarySearch2(nums, target);
         System.out.println("目标元素6的索引 = " + index);
     }
 
